752ce0640f26228c97b3079b73361d852e25f816
masterwebdev <masterwebdev@gmail.com>
2017-05-13 02:43:35 +0300

    f

--- src/ios/AVStore.m
+++ src/ios/AVStore.m
@@ -27,34 +27,15 @@
 {
     CDVPluginResult* pluginResult = nil;
     NSString* path = [command.arguments objectAtIndex:0];
-    NSString* start_position = [command.arguments objectAtIndex:1];
-    NSString* end_position = [command.arguments objectAtIndex:1];
     
-    int start_pos=[start_position intValue];
-    int end_pos=[end_position intValue];
-    
-    
-    NSLog(@"PATHHHHHHHHHHH: %@", path);
-    
-    NSURL *url = [NSURL URLWithString:path];
-    
-    AVAsset *asset = [AVAsset assetWithURL:url];
-    AVAssetImageGenerator *imageGenerator = [[AVAssetImageGenerator alloc]initWithAsset:asset];
-    CMTime time = [asset duration];
-    time.value = 1 ;
-    CGImageRef imageRef = [imageGenerator copyCGImageAtTime:time actualTime:NULL error:NULL];
-    UIImage *image1 = [UIImage imageWithCGImage:imageRef];
-    
-    NSData *imageData = UIImageJPEGRepresentation(image1, 1.0);
-    
-    NSLog(@"%@", imageData);
-    
-    NSString *base64String = [imageData base64EncodedStringWithOptions:0];
-    NSLog(@"BASE64: %@", base64String);
+    if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(path))
+    {
+        UISaveVideoAtPathToSavedPhotosAlbum(path, self, nil, nil);
+    }
     
     CDVPluginResult* result = [CDVPluginResult
                                resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:base64String];
+                               messageAsString:path];
     
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
